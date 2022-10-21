9a68b81b08a79be286032599072f9e3112224f9c
sebax <sebaxtian.tc@gmail.com>
2022-10-21 14:40:05 -0500

    Initial Commit

---
+++ PokedexTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PokedexTest
+//
+//  Created by Sebastian TC on 21/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokedexTest/PokedexTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexTestApp.swift
+//  PokedexTest
+//
+//  Created by Sebastian TC on 21/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
