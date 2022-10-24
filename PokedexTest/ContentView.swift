4d3763160fae7b8bff1e7fadb4ba20d7ac5eea90
sebax <sebaxtian.tc@gmail.com>
2022-10-23 22:04:59 -0500

    search

--- PokedexTest/ContentView.swift
+++ PokedexTest/ContentView.swift
@@ -1,9 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.isSearching) var isSearching
     @ObservedObject var viewModel = PokemonViewModel()
     @State var typeName = ""
     @State private var searchText = ""
+    @State var loaded: Bool = true
+    @State var searchResults: [PokemonList] = []
+    @State var suggestedSearches: [String] = []
+    @State var searchByType: Bool = false
 
     
     var body: some View {
@@ -18,7 +23,28 @@ struct ContentView: View {
             }
             .navigationTitle("Pokedex")
             .searchable(text: $searchText)
-            
+            .onSubmit(of: .search) {
+                if searchText.isEmpty {
+                    searchResults = []
+                }
+                else {
+                    DispatchQueue.main.async {
+                        loaded = false
+                    }
+                    if searchByType {
+                        self.viewModel.searchByType(type: searchText)
+                    }
+                    else {
+                        self.viewModel.searchByName(name: searchText)
+                    }
+                }
+            }
+            .onChange(of: isSearching) { newValue in
+                if !newValue {
+                    viewModel.pokemonList.removeAll()
+                    searchResults = []
+                }
+            }
         }
         .onAppear(perform: viewModel.fetchData)
     }
--- PokedexTest/ViewModel/PokemonViewModel.swift
+++ PokedexTest/ViewModel/PokemonViewModel.swift
@@ -21,21 +21,31 @@ class PokemonViewModel: ObservableObject {
     }
     
     func searchByName(name: String) {
+        let urlString = "https://pokeapi.co/api/v2/pokemon/\(name.lowercased())"
+        
+        guard let url = URL(string: urlString) else { return }
+        
         let request = URLRequest(url: url)
         
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let data = data {
-                if let decoded = try? JSONDecoder().decode(Result.self, from: data) {
+                var pokeData = [String: Any]()
+                do {
+                    pokeData = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String: Any]
+                    let pokelist = PokemonList(name: pokeData["name"] as! String, url: "https://pokeapi.co/api/v2/pokemon/\(pokeData["id"] as! Int)")
                     DispatchQueue.main.async {
-                        self.pokemonList = decoded.results
+                        self.pokemonList.removeAll()
+                        self.pokemonList.append(pokelist)
                     }
+                } catch let error as NSError {
+                    print(error)
                 }
             }
         }.resume()
     }
     
     func searchByType(type: String) {
-        let request = URLRequest(url: url)
+        let request = URLRequest(url: URL(string: "https://pokeapi.co/api/v2/type/" + type + "/")!)
         
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let data = data {
